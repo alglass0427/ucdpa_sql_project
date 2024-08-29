143984ac6393a3ebd5de8b19ff1b6a8f108ecc4b
alglass0427 <144165219+alglass0427@users.noreply.github.com>
2024-08-29 19:44:37 +0100

    added modal fix  -  added back render DB

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,8 @@ def create_app():
     app = Flask(__name__)
     # basedir = os.path.abspath(os.path.dirname(__file__))   # C:Users/.../..../....
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Holly#040115@localhost/portfolio_db'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://alwglass:G4MaiivKjlB6fpgdbD9WlHLlgeDBPCr9@dpg-cr735l3tq21c73f800cg-a.oregon-postgres.render.com/portfolio_db_5uxo'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Holly#040115@localhost/portfolio_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://alwglass:G4MaiivKjlB6fpgdbD9WlHLlgeDBPCr9@dpg-cr735l3tq21c73f800cg-a.oregon-postgres.render.com/portfolio_db_5uxo'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.secret_key = 'mysecretkey'
     CURRENT_DIR =  os.getcwd()
